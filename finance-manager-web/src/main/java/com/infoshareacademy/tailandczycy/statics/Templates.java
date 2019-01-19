8829cd4e088a1ae4b71ba8fb74890435fee838c8
MartinFrac <marcin.jarek.frackiewicz@gmail.com>
2019-01-19 16:11:15 +0100

    adding static names for templates

---
+++ finance-manager-web/src/main/java/com/infoshareacademy/tailandczycy/statics/Templates.java
@@ -0,0 +1,11 @@
+package com.infoshareacademy.tailandczycy.statics;
+
+public class Templates {
+
+    public static final String HOME = "/home";
+    public static final String EDIT_EXPENSE = "/edit-expense";
+    public static final String EDIT_CATEGORY = "/edit-category";
+    public final static String DELETE_CATEGORY = "/delete-category";
+    public static final String ADD_EXPENSE = "/add-expense";
+    public static final String ADD_CATEGORY = "/add-category";
+}
