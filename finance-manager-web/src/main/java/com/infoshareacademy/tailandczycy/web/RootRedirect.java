88b64329ab5b3f8630142e3977e9cc6825696922
MartinFrac <marcin.jarek.frackiewicz@gmail.com>
2019-01-24 14:00:27 +0100

    creating redirect to home

--- finance-manager-web/src/main/java/com/infoshareacademy/tailandczycy/web/RootRedirect.java
+++ finance-manager-web/src/main/java/com/infoshareacademy/tailandczycy/web/RootRedirect.java
@@ -1,4 +1,17 @@
 package com.infoshareacademy.tailandczycy.web;
 
-public class RootRedirect {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/")
+public class RootRedirect extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect("/home");
+    }
 }
