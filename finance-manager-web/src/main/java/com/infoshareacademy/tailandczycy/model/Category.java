231a416185a803c37e37abe78a0a6f3aad27c3ad
MartinFrac <marcin.jarek.frackiewicz@gmail.com>
2019-02-11 16:20:44 +0100

    deleting import

--- finance-manager-web/src/main/java/com/infoshareacademy/tailandczycy/model/Category.java
+++ finance-manager-web/src/main/java/com/infoshareacademy/tailandczycy/model/Category.java
@@ -1,7 +1,5 @@
 package com.infoshareacademy.tailandczycy.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
