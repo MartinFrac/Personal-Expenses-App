c37c3c833cbea3b83d2fd17d48a700fa87fbba8a
MartinFrac <marcin.jarek.frackiewicz@gmail.com>
2019-02-11 16:09:16 +0100

    deleting useless code

--- finance-manager-web/src/main/java/com/infoshareacademy/tailandczycy/web/AddCategory.java
+++ finance-manager-web/src/main/java/com/infoshareacademy/tailandczycy/web/AddCategory.java
@@ -1,8 +1,6 @@
 package com.infoshareacademy.tailandczycy.web;
 
-import com.infoshareacademy.tailandczycy.cdi.CategoryBean;
 import com.infoshareacademy.tailandczycy.cdi.TemplateBean;
-import com.infoshareacademy.tailandczycy.dto.CategoryDto;
 import com.infoshareacademy.tailandczycy.statics.Templates;
 
 import javax.inject.Inject;
@@ -13,13 +11,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Map;
 
 @WebServlet(urlPatterns = "add-category")
 public class AddCategory extends HttpServlet {
-
-    @Inject
-    CategoryBean categoryBean;
 
     @Inject
     TemplateBean templateBean;
@@ -28,13 +22,5 @@ public class AddCategory extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HashMap<String, Object> dataModel = new HashMap<>();
         templateBean.handleTemplate(dataModel, Templates.ADD_CATEGORY, resp, getServletContext());
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        CategoryDto categoryRequestView = categoryBean.getCategoryDto(req);
-        Map<String, Object> dataModel = new HashMap<>();
-        dataModel.put("category", categoryRequestView);
-        templateBean.handleTemplate(dataModel, Templates.HOME, resp, getServletContext());
     }
 }
