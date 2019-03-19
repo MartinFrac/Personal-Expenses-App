0738051154aeb2f1ca2774390bea3b05bb6bfe82
MartinFrac <marcin.jarek.frackiewicz@gmail.com>
2019-03-19 19:41:06 +0100

    displaying categories checkboxes by js in add-expense servlet

--- finance-manager-web/src/main/webapp/scripts/getCategories.js
+++ finance-manager-web/src/main/webapp/scripts/getCategories.js
@@ -1,9 +1,19 @@
 (function () {
+    var categoryContainer = document.getElementById("categoryContainer");
     var ourRequest = new XMLHttpRequest();
     ourRequest.open('GET', 'http://localhost:8080/resources/categories');
     ourRequest.onload = function() {
         var ourData = JSON.parse(ourRequest.responseText);
-        console.log(ourData[0]);
+        var htmlString = "";
+        for(var i = 0; i< ourData.length; i++) {
+            htmlString +=
+                "<label class='containerCategory'>" + ourData[i].name +
+                    "<input type='checkbox'>" +
+                    "<span class='checkmark'></span>" +
+                "</label>";
+        }
+
+        categoryContainer.insertAdjacentHTML('beforeend', htmlString);
     };
     ourRequest.send();
 })();
