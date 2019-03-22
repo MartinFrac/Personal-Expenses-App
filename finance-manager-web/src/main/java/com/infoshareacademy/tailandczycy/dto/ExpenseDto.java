1cf717948203f4fa5c9c6f33dd28fa926c8fb8fd
MartinFrac <marcin.jarek.frackiewicz@gmail.com>
2019-03-22 18:07:39 +0100

    cleaning up

--- finance-manager-web/src/main/java/com/infoshareacademy/tailandczycy/dto/ExpenseDto.java
+++ finance-manager-web/src/main/java/com/infoshareacademy/tailandczycy/dto/ExpenseDto.java
@@ -20,9 +20,7 @@ public class ExpenseDto {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
 
     public void setName(String name) {
         this.name = name;
