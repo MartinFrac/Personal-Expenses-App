98b8c24bc371bb0ee231397f8702c283a1a3ac1e
MartinFrac <marcin.jarek.frackiewicz@gmail.com>
2019-03-22 19:20:51 +0100

    adjusting code for changes with date string in expenseDto and deleting get dto method in expenseBean

--- finance-manager-web/src/main/java/com/infoshareacademy/tailandczycy/cdi/ExpenseBean.java
+++ finance-manager-web/src/main/java/com/infoshareacademy/tailandczycy/cdi/ExpenseBean.java
@@ -8,13 +8,7 @@ import com.infoshareacademy.tailandczycy.dto.ExpenseDto;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
 
 @RequestScoped
@@ -23,19 +17,6 @@ public class ExpenseBean {
     private ExpenseDao expenseDao;
     @Inject
     private CategoryDao categoryDao;
-
-    public ExpenseDto getExpenseDto(HttpServletRequest req) {
-        ExpenseDto expenseDto = new ExpenseDto();
-        List<String> categoriesString = new ArrayList<>(Arrays.asList(req.getParameterValues("categories")));
-
-        expenseDto.setName(req.getParameter("name"));
-        expenseDto.setComment(req.getParameter("comment"));
-        expenseDto.setAmount(parseStringToBigDecimal(req.getParameter("amount")));
-        expenseDto.setDate(req.getParameter("date"));
-        expenseDto.setCategories(categoriesString);
-
-        return expenseDto;
-    }
 
     public Optional<ExpenseDto> getExpenseById(Long id) {
 
@@ -64,11 +45,7 @@ public class ExpenseBean {
         expenseDao.save(expense);
     }
 
-    public LocalDate parseStringToLocalDate(String date) {
+    private LocalDate parseStringToLocalDate(String date) {
         return LocalDate.parse(date);
-    }
-
-    private BigDecimal parseStringToBigDecimal(String param) {
-        return new BigDecimal(param).setScale(2, RoundingMode.HALF_UP);
     }
 }
