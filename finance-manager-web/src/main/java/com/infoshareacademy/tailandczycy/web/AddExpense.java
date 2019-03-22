a698624e87baeb04e496e151daf087935ca341d4
MartinFrac <marcin.jarek.frackiewicz@gmail.com>
2019-03-22 19:24:02 +0100

    setting up expenseDto in addExpense

--- finance-manager-web/src/main/java/com/infoshareacademy/tailandczycy/web/AddExpense.java
+++ finance-manager-web/src/main/java/com/infoshareacademy/tailandczycy/web/AddExpense.java
@@ -1,10 +1,13 @@
 package com.infoshareacademy.tailandczycy.web;
 
 import com.infoshareacademy.tailandczycy.cdi.TemplateBean;
+import com.infoshareacademy.tailandczycy.mappers.ExpenseDtoMapper;
+import com.infoshareacademy.tailandczycy.model.Expense;
 import com.infoshareacademy.tailandczycy.statics.Templates;
 import com.infoshareacademy.tailandczycy.validations.Validator;
 import com.infoshareacademy.tailandczycy.cdi.ExpenseBean;
 import com.infoshareacademy.tailandczycy.dto.ExpenseDto;
+import org.apache.logging.log4j.core.config.plugins.convert.TypeConverters;
 
 import javax.inject.Inject;
 import javax.servlet.annotation.WebServlet;
@@ -12,6 +15,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Arrays;
 import java.util.HashMap;
 
 @WebServlet(urlPatterns = "/add-expense")
@@ -36,8 +42,12 @@ public class AddExpense extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         HashMap<String, Object> dataModel = new HashMap<>();
         if(validator.isExpenseCorrect(req)) {
-            //TODO create expense here and pass as parameter to map to dto
-            ExpenseDto expenseDto = expenseBean.getExpenseDto(req);
+            ExpenseDto expenseDto = new ExpenseDto();
+            expenseDto.setName(req.getParameter("name"));
+            expenseDto.setComment(req.getParameter("comment"));
+            expenseDto.setAmount(new BigDecimal(req.getParameter("amount")).setScale(2, RoundingMode.HALF_UP));
+            expenseDto.setDate(req.getParameter("date"));
+            expenseDto.setCategories(Arrays.asList(req.getParameterValues("categories")));
             expenseBean.saveExpense(expenseDto);
             dataModel.put("state", "added");
             templateBean.handleTemplate(dataModel, Templates.HOME, resp, getServletContext());
