dd256d942e1da7dbbfa3c761286105de0bad4a7f
MartinFrac <marcin.jarek.frackiewicz@gmail.com>
2019-03-25 12:47:08 +0100

    ExpenseBean- getAllExpenseDtos method added

--- finance-manager-web/src/main/java/com/infoshareacademy/tailandczycy/cdi/ExpenseBean.java
+++ finance-manager-web/src/main/java/com/infoshareacademy/tailandczycy/cdi/ExpenseBean.java
@@ -2,11 +2,14 @@ package com.infoshareacademy.tailandczycy.cdi;
 
 import com.infoshareacademy.tailandczycy.dao.CategoryDao;
 import com.infoshareacademy.tailandczycy.dao.ExpenseDao;
+import com.infoshareacademy.tailandczycy.mappers.ExpenseDtoMapper;
 import com.infoshareacademy.tailandczycy.model.Expense;
 import com.infoshareacademy.tailandczycy.dto.ExpenseDto;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequestScoped
 public class ExpenseBean {
@@ -14,6 +17,14 @@ public class ExpenseBean {
     private ExpenseDao expenseDao;
     @Inject
     private CategoryDao categoryDao;
+    @Inject
+    ExpenseDtoMapper expenseDtoMapper;
+
+    public List<ExpenseDto> getAllExpenseDtos() {
+        return expenseDao.findAll().stream()
+                .map(e -> expenseDtoMapper.mapToDto(e))
+                .collect(Collectors.toList());
+    }
 
     public void saveExpense(ExpenseDto expenseDto) {
         Expense expense = new Expense();
